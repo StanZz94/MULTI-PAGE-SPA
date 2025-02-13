9493cdbc365f1baa66bf6df62efddf0416e133c1
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-02-13 23:54:03 +0200

    create base Home component and define route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+createBrowserRouter([
+  {path:'/', element: <Home />},
+  {},
+  {},
+]);
+
 function App() {
   return <div></div>;
 }
