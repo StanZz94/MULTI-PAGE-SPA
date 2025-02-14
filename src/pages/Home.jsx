e512f5f480ae37f1706ddaecf204a2d322313515
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-02-14 23:02:46 +0200

    add Link in Home to Products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,9 @@
+import { Link } from "react-router-dom"
+
 export default function Home() {
-    
-    return <h1>Home Page</h1>
+
+    return <>
+    <h1>Home Page</h1>
+    <p>Go to products <Link to="/products">Here</Link></p>
+    </>
 }
