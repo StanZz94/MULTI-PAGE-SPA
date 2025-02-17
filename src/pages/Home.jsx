bd2b6a9e937ac61ad066072760af1dfd5eaf7e2a
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-02-17 23:19:33 +0200

    final formatting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Home() {
 
     return <>
-    <h1>Home Page</h1>
-    <p>Go to products <Link to="/products">Here</Link></p>
+        <h1>Home Page</h1>
+        <p>Go to products <Link to="/products">Here</Link></p>
     </>
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export default function Products() {
 
     const PRODUCTS = [
-        {id: 'p1', title: 'Product 1'},
-        {id: 'p2', title: 'Product 2'},
-        {id: 'p3', title: 'Product 3'},
-        {id: 'p4', title: 'Product 4'},
+        { id: 'p1', title: 'Product 1' },
+        { id: 'p2', title: 'Product 2' },
+        { id: 'p3', title: 'Product 3' },
+        { id: 'p4', title: 'Product 4' },
     ];
 
     return <>
@@ -15,7 +15,7 @@ export default function Products() {
             {PRODUCTS.map((product) => (
                 <li key={product.id}>
                     <Link to={`/products/${product.id}`}>{product.title}</Link>
-                    </li>
+                </li>
             ))}
         </ul>
     </>
